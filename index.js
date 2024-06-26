acb07fe190f6beb4e57dc49d8482830d5c8616c8
WAHU0508 <sharongikenye@gmail.com>
2024-06-26 15:43:32 +0300

    Adding arithmetic equations

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+//test-1: Multiply num1 and num2 to get 62
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+console.log(multiply);
 
-// Write your code here
+//test-2: Generate random integer greater than 0
+let random = Math.ceil(Math.random());
+console.log(random);
+
+//test-3: Equation to calculate remainder that is equal to 4
+let num3 = 68;
+let num4 = 16;
+let mod = num3 % num4;
+console.log(mod);
+
+//test-4: Return 20 as highest number in set
+let max = Math.max(1, 0, 20, 16, 8);
+console.log(max);
